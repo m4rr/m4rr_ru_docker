4640f6b631571ed7e93c4e0676c20074185529d3
Marat Saytakov <remarr@gmail.com>
2019-08-20 00:10:47 +0300

    clean up

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,16 @@
-cd ~/m4rr_ru_docker
-docker-compose down && git pull && docker-compose build && docker-compose up -d
-
-cd ~/blog-m4rr-deploy
-docker-compose down && git pull && docker-compose build && docker-compose up -d
-
 docker stop nginx-proxy
 docker rm nginx-proxy
 
 docker run -d -p 80:80 --restart=unless-stopped --name nginx-proxy -e ENABLE_IPV6=true -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
 
+cd ~/m4rr_ru_docker
+docker-compose down && git pull && docker-compose build && docker-compose up -d
+
+cd ~
 docker network connect m4rrrudocker_default nginx-proxy
 
+cd ~/blog-m4rr-deploy
+docker-compose down && git pull && docker-compose build --no-cache && docker-compose up -d
+
+cd ~
 docker network connect blogm4rrdeploy_default nginx-proxy
