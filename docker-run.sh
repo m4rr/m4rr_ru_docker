663ec9de951e05b7d0891cbcfe54570264adf71c
Marat Saytakov <remarr@gmail.com>
2019-08-20 00:57:54 +0300

    docker start nginx-proxy

--- docker-run.sh
+++ docker-run.sh
@@ -6,21 +6,23 @@ git pull && docker-compose build
 
 cd ~
 docker stop nginx-proxy
-docker rm nginx-proxy
-docker run -d -p 80:80 --restart=unless-stopped --name nginx-proxy -e ENABLE_IPV6=true -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+
+# to build from scratch:
+# docker run -d -p 80:80 --rm --restart=unless-stopped --name nginx-proxy -e ENABLE_IPV6=true -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+# https://stackoverflow.com/a/52989931
 
 cd ~/m4rr_ru_docker
 docker-compose down
 
 cd ~/blog-m4rr-deploy
 docker-compose down
+docker-compose up -d
+
+cd ~/m4rr_ru_docker
+docker-compose up -d
 
 cd ~
 docker network connect m4rrrudocker_default nginx-proxy
 docker network connect blogm4rrdeploy_default nginx-proxy
 
-cd ~/m4rr_ru_docker
-docker-compose up -d
-
-cd ~/blog-m4rr-deploy
-docker-compose up -d
+docker start nginx-proxy
